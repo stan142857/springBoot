74e96d7aab0d11f3578c47a1be57f08f1d1ce4a8
stan142857 <stan142857@outlook.com>
2021-05-13 14:37:49 +0800

    init

---
+++ src/main/java/salamander/standstill/demo/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package salamander.standstill.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World1") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ src/test/java/salamander/standstill/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package salamander.standstill.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.printf("aa");
+    }
+
+}
